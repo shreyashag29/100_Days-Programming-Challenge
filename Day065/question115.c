1cb5b33cb067b2c0c2202e8ef87df402b2b2e196
Shreyash Agarwal <shreyashagarwal36@gmail.com>
2025-12-02 23:19:45 +0530

    main

--- Day065/question115.c
+++ Day065/question115.c
@@ -1,3 +1,18 @@
+//Q115: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
+
+/*
+Sample Test Cases:
+Input 1:
+s = "anagram", t = "nagaram"
+Output 1:
+Anagram
+
+Input 2:
+s = "rat", t = "car"
+Output 2:
+Not Anagram
+
+*/
 #include <stdio.h>
 #include <string.h>
 
@@ -24,4 +39,5 @@ void main() {
         }
     }
     printf("Anagram");
+
 }
