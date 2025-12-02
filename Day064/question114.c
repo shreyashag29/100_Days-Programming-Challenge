9adbe43ff7d0414e2e4eb3d62da7a2cba0b9b84a
Shreyash Agarwal <shreyashagarwal36@gmail.com>
2025-12-02 23:19:16 +0530

    main
    The program calculates the length of the longest substring without repeating characters. Sample test cases are included in comments.

--- Day064/question114.c
+++ Day064/question114.c
@@ -1,3 +1,23 @@
+//Q114: Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
+
+/*
+Sample Test Cases:
+Input 1:
+s = "abcabcbb"
+Output 1:
+3
+
+Input 2:
+s = "bbbbb"
+Output 2:
+1
+
+Input 3:
+s = "pwwkew"
+Output 3:
+3
+
+*/
 #include <stdio.h>
 #include <string.h>
 
@@ -16,4 +36,5 @@ void main() {
         if(i - start + 1 > max_len) max_len = i - start + 1;
     }
     printf("%d", max_len);
+
 }
